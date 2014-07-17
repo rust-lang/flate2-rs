7b130d27de0a13297fec8b24533f89d64d6f297b
Alex Crichton <alex@alexcrichton.com>
2014-07-17 14:41:21 -0700

    Enable cross-compilation to 32-bit

--- cargo.mk
+++ cargo.mk
@@ -1,3 +1,9 @@
+ifneq ($(findstring i686,$(TARGET)),)
+	CC := $(CC) -m32
+else
+	CC := $(CC) -fPIC
+endif
+
 all:
 	$(CC) src/miniz.c -c -o $(DEPS_DIR)/miniz.o
 	ar crus $(DEPS_DIR)/libminiz.a $(DEPS_DIR)/miniz.o
