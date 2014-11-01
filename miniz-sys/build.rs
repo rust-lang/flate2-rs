f4cee48bc1e82fc6d8278147e661c8057c7be464
Alex Crichton <alex@alexcrichton.com>
2014-11-01 15:42:52 -0700

    Update gcc-rs

--- miniz-sys/build.rs
+++ miniz-sys/build.rs
@@ -1,5 +1,7 @@
 extern crate gcc;
 
+use std::default::Default;
+
 fn main() {
-    gcc::compile_library("libminiz.a", ["miniz.c"]);
+    gcc::compile_library("libminiz.a", &Default::default(), ["miniz.c"]);
 }
