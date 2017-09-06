a428791ae67308d92b8d720d0a30a8e57d5b2645
Alex Crichton <alex@alexcrichton.com>
2017-09-06 07:14:01 -0700

    Return an error when `write` returns 0 bytes
    Closes #123

--- src/zio.rs
+++ src/zio.rs
@@ -133,6 +133,9 @@ impl<W: Write, D: Ops> Writer<W, D> {
         // a deque-like strategy.
         while self.buf.len() > 0 {
             let n = try!(self.obj.as_mut().unwrap().write(&self.buf));
+            if n == 0 {
+                return Err(io::ErrorKind::WriteZero.into())
+            }
             self.buf.drain(..n);
         }
         Ok(())
