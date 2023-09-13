013e79de11c7006e39fb5ec1ae7df9d2a7968977
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-09-13 11:04:20 +0200

    cargo fmt

--- src/zlib/read.rs
+++ src/zlib/read.rs
@@ -222,11 +222,10 @@ impl<R> ZlibDecoder<R> {
         self.inner.get_mut().reset(r)
     }
 
-
     /// Resets the state of the decoder entirely
     /// Opposed to `reset`, this function takes into account that there was
     /// previously a custom Decompress in place
-    /// 
+    ///
     /// `zlib_header` should be whatever was used before in `new_with_decompress`
     pub fn reset_on_custom(&mut self, r: R, zlib_header: bool) -> R {
         self.inner.get_data_mut().reset(zlib_header);
