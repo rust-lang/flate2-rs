c0f1163d044258ed029f59db6c307ae745e2da51
Sebastian Thiel <sebastian.thiel@icloud.com>
2025-12-12 05:05:24 +0100

    refactor - remove unnecessary newlines

--- src/ffi/zlib_rs.rs
+++ src/ffi/zlib_rs.rs
@@ -242,12 +242,9 @@ impl Deflate {
         match self.inner.set_level(level.level() as i32) {
             Ok(status) => match status {
                 Status::Ok => Ok(()),
-
                 Status::BufError => compress_failed(ErrorMessage(Some("insufficient space"))),
-
                 Status::StreamEnd => unreachable!(),
             },
-
             Err(_) => self.compress_error(),
         }
     }
